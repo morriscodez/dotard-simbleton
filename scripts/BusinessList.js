a668e7ecd69520f9a212aceb501ee2241541e87b
Dylan Morris <dylanrobertmorris@gmail.com>
2021-09-14 11:21:57 -0500

    added script tag to index.html

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,1 +1,15 @@
-import { useBusinesses } from "./BusinessProvider";
+import { useBusinesses } from "./BusinessProvider.js";
+import { Business } from "./Business.js";
+
+const contentTarget = document.querySelector(".businesses")
+
+const BusinessList = () => {
+    const businessArray = useBusinesses()
+    contentTarget.innerHTML = "<h1> Businesses </h1>"
+
+    businessArray.forEach(
+        (businessObject) => {
+            contentTarget.innerHTML += Business(businessObject)
+        }
+    )
+}
