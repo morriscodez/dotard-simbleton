c57c5ae8a358f00fb09413aa4e9aa55bbb6ab73a
Dylan Morris <dylanrobertmorris@gmail.com>
2021-09-14 11:09:34 -0500

    changed property names in Business.js

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,9 +1,11 @@
 export const Business = (businessObj) => {
     return `
     <section class="business">
-        <h2 class="business_type">${businessObj.type}</h2>
-        <div class="business_price">
-            Price: ${businessObj.price}
+        <h2 class="business_companyName">${businessObj.companyName}</h2>
+        <div class="business_address">
+            ${businessObj.addressFullStreet}
+            ${businessObj.addressStateCode}
+            ${businessObj.addressZipCode}
         </div>
-</section>`
+    </section>`
 }
