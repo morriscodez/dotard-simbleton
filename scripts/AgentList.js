b41793eed65cefbe5c38e80dcb5fc09885a4b077
Dylan Morris <dylanrobertmorris@gmail.com>
2021-09-15 10:55:08 -0500

    finished compiling code, need to check and debug

--- scripts/AgentList.js
+++ scripts/AgentList.js
@@ -12,4 +12,36 @@ export const PurchaseAgentList = () => {
     agentArray.forEach(agent => {
         agentHTML =+ Agent(agent)
     })
+
+    contentTarget.innerHTML += `<h2 class="purchase-agent-title">Purchasing Agent </h2>
+                                ${agentHTML}`
+
+    // search functionality, agents
+
+    const agentSearchResultArticle = document.querySelector(".foundAgents")
+
+    document
+        .querySelector("#agentSearch")
+        .addEventListener("keypress", keyPressEvent => {
+            if (keyPressEvent.charCode === 13) {
+                const foundAgent = []
+                const searchKey = keyPressEvent.target.value.toLowerCase()
+
+                if (searchKey === "") {
+                    agentSearchResultArticle.innerHTML = ""
+                    contentTarget.innerHTML = ""
+                    return PurchaseAgentList()
+                }
+                foundAgent.push(agentArray.find(agent => {
+                    return agent.fullName.toLowerCase().includes(searchKey)
+                }))
+                console.log(foundAgent)
+                let foundAgentHTML = ""
+                foundAgent.forEach(agent => {
+                    foundAgentHTML += PurchaseAgentList(agent)
+                })
+                contentTarget.innerHTML = ""
+                agentSearchResultArticle.innerHTML = foundAgentHTML
+            }
+        })
 }
